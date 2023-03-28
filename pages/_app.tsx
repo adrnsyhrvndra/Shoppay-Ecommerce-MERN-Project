bdf74c7dbc51b70c8b54d09dfa3a8e0d6e029fb7
Adriansyah Ravindra <adrimedia.indonesia@gmail.com>
2023-03-28 17:31:31 +0700

    Revert "Next Auth JS Setup"
    This reverts commit e4e346f1153d220d9f0cf6675717bd92572773b4.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,14 +5,12 @@ import store from "../store";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 import Head from "next/head";
-import { SessionProvider } from "next-auth/react"
 
 let persistor = persistStore(store);
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-
       <Head>
         <title>Shoppay</title>
         <meta name="description" content="Shoppay Online Shooping Sevice" />
@@ -20,14 +18,11 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <SessionProvider session={session}>
-        <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
-            <Component {...pageProps} />
-          </PersistGate>
-        </Provider>
-      </SessionProvider>
-
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,20 +5,13 @@ import styles from '@/styles/Home.module.scss';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
 import axios from 'axios';
-import { useSession, signIn, signOut } from "next-auth/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home({country}) {
-
-  const { data: session } = useSession()
-
   return (
     <div>
       <Header country={country}/>
-      {
-        session ? "You Are Logged In" : "You Are Not Logged In"
-      }
       <Footer country={country}/>
     </div>
   );
